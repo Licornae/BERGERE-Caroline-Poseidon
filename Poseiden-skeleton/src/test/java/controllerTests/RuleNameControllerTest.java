018b6a5daace36b3c6789fde29688358c286054a
Caroline <bergere.caroline@outlook.fr>
2026-03-30 22:04:41 +0200

    Add tests for RuleNameController validation, update, and delete actions

--- Poseiden-skeleton/src/test/java/controllerTests/RuleNameControllerTest.java
+++ Poseiden-skeleton/src/test/java/controllerTests/RuleNameControllerTest.java
@@ -48,4 +48,112 @@ public class RuleNameControllerTest {
                 .andExpect(model().attributeExists("ruleNames"))
                 .andExpect(model().attribute("ruleNames", mockRuleNames));
     }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    public void validate_withValidData_shouldSaveAndRedirect() throws Exception {
+        RuleName ruleName = new RuleName(
+                null,
+                "Rule1",
+                "Description1",
+                "Json1",
+                "Template1",
+                "SqlStr1",
+                "SqlPart1");
+
+        Mockito.when(ruleNameService.save(Mockito.any(RuleName.class))).thenReturn(ruleName);
+
+        mockMvc.perform(post("/ruleName/validate")
+                        .param("name", "Rule1")
+                        .param("description", "Description1")
+                        .param("json", "Json1")
+                        .param("template", "Template1")
+                        .param("sqlStr", "SqlStr1")
+                        .param("sqlPart", "SqlPart1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/ruleName/list"));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    public void validate_withInvalidData_shouldReturnAddView() throws Exception {
+        mockMvc.perform(post("/ruleName/validate")
+                        .param("name", "")
+                        .param("description", "Description1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("ruleName/add"))
+                .andExpect(model().attributeHasFieldErrors("ruleName", "name"));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    public void showUpdateForm_shouldReturnUpdateViewWithRuleName() throws Exception {
+        RuleName mockRuleName = new RuleName(1, "Rule1", "Description1", "Json1", "Template1", "SqlStr1", "SqlPart1");
+
+        Mockito.when(ruleNameService.findById(1)).thenReturn(java.util.Optional.of(mockRuleName));
+
+        mockMvc.perform(get("/ruleName/update/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("ruleName/update"))
+                .andExpect(model().attributeExists("ruleName"))
+                .andExpect(model().attribute("ruleName", mockRuleName));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    public void updateRuleName_withValidData_shouldUpdateAndRedirect() throws Exception {
+        RuleName updatedRuleName = new RuleName(
+                1,
+                "UpdatedRule",
+                "UpdatedDescription",
+                "UpdatedJson",
+                "UpdatedTemplate",
+                "UpdatedSqlStr",
+                "UpdatedSqlPart");
+
+        Mockito.when(ruleNameService.save(Mockito.any(RuleName.class))).thenReturn(updatedRuleName);
+
+        mockMvc.perform(post("/ruleName/update/1")
+                        .param("name", "UpdatedRule")
+                        .param("description", "UpdatedDescription")
+                        .param("json", "UpdatedJson")
+                        .param("template", "UpdatedTemplate")
+                        .param("sqlStr", "UpdatedSqlStr")
+                        .param("sqlPart", "UpdatedSqlPart"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/ruleName/list"));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    public void updateRuleName_withInvalidData_shouldReturnUpdateView() throws Exception {
+        mockMvc.perform(post("/ruleName/update/1")
+                        .param("name", "")
+                        .param("description", "UpdatedDescription"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("ruleName/update"))
+                .andExpect(model().attributeHasFieldErrors("ruleName", "name"));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    public void deleteRuleName_shouldDeleteAndRedirect() throws Exception {
+        RuleName mockRuleName = new RuleName(
+                1, "Rule1",
+                "Description1",
+                "Json1",
+                "Template1",
+                "SqlStr1",
+                "SqlPart1");
+
+        Mockito.when(ruleNameService.findById(1)).thenReturn(java.util.Optional.of(mockRuleName));
+        Mockito.doNothing().when(ruleNameService).deleteById(1);
+
+        mockMvc.perform(get("/ruleName/delete/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/ruleName/list"));
+
+        Mockito.verify(ruleNameService, Mockito.times(1)).deleteById(1);
+    }
+
 }
